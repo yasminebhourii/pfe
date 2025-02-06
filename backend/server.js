f6a1490f394a87cebb1c67fd16ed49cb0a80f19b
yasminebh <bhouriyasmine9@gmail.com>
2025-02-06 20:32:19 +0100

    Normalized line endings

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+
+.then(() => console.log("MongoDB connecté"))
+.catch(err => console.log(err));
+
+app.use('/api/users', userRoutes);
+
+app.listen(process.env.PORT, () => console.log(`Serveur lancé sur http://localhost:${process.env.PORT}`));
